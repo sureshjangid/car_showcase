5063d2d1ecc5e6290955e1a1012ad80a3b9eb1cc
suresh jangid <sureshjangir902@gmail.com>
2023-11-02 07:56:20 +0530

    update

--- types/index.ts
+++ types/index.ts
@@ -4,5 +4,5 @@ export interface CustomButtomProps {
   title: string;
   contrainerStyle?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType?:string
+  btnType?: "button" | "submit";
 }
